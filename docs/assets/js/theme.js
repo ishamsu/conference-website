55774e7c601eb99985792a97d1a5f9beb4260fa6
ishamsu <shamsu1997@gmail.com>
2020-05-26 15:21:40 +0530

    💬 added tooltip to whatsapp-icon

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -1,12 +1,25 @@
 // Custome theme code
 
 if ($('.clean-gallery').length > 0) {
-   baguetteBox.run('.clean-gallery', { animation: 'slideIn'});
+    baguetteBox.run('.clean-gallery', { animation: 'slideIn' });
 }
 
 if ($('.clean-product').length > 0) {
-    $(window).on("load",function() {
+    $(window).on("load", function() {
         $('.sp-wrap').smoothproducts();
     });
 }
-
+$(document).ready(function() {
+    $(".tip-top").tooltip({
+        placement: 'top'
+    });
+    $(".tip-right").tooltip({
+        placement: 'right'
+    });
+    $(".tip-bottom").tooltip({
+        placement: 'bottom'
+    });
+    $(".tip-left").tooltip({
+        placement: 'left'
+    });
+});
