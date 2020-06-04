e0eb84e730fa7fca2a23a8ccab841a10c4075dc2
ishamsu <shamsu1997@gmail.com>
2020-06-04 11:24:44 +0530

    ⚡️ added scrolling text

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -23,3 +23,17 @@ $(document).ready(function() {
         placement: 'left'
     });
 });
+
+jQuery(document).ready(function($) {
+    var promoticker = function() {
+        var window_width = window.innerWidth;
+        var speed = 8 * window_width;
+        $('#promo-notifications li:first').animate({ left: '-980px' }, speed, 'linear', function() {
+            $(this).detach().appendTo('#promo-notifications ul').css('left', "100%");
+            promoticker();
+        });
+    };
+    if ($("#promo-notifications li").length > 1) {
+        promoticker();
+    }
+});
